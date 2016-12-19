32ac90210a4381b712825248e9a2f295ec1824f7
Pranavi Adusumilli <pranaviadusumilli@hotmail.com>
2016-12-19 11:36:28 -0500

    File FixUp!!

---
+++ minute-masterpiece 2/MEAPhotoEditor/Masterpiece.swift
@@ -0,0 +1,15 @@
+//
+//  Masterpiece.swift
+//  MEAPhotoEditor
+//
+//  Created by Pranavi Adusumilli  on 12/16/16.
+//  Copyright © 2016 MeaMobile. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Masterpiece {
+    var captionField: String!
+    var filteredImage: UIImage!
+}
